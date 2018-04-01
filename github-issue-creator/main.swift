439f301efcf7ef162941cf77d2df301c27724747
Blake Merryman <blake.merryman@gmail.com>
2018-04-01 11:44:16 -0500

    Initial Commit

---
+++ github-issue-creator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  github-issue-creator
+//
+//  Created by Blake Merryman on 4/1/18.
+//  Copyright © 2018 Merryman Technology. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
